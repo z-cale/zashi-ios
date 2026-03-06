a24d309a6d06bb192042d7996f22531f1a4755e2
Greg Nagy <greg@dhamma.works>
2026-03-06 20:48:03 +0100

    Use fork dependencies for voting (revert after upstream)
    Points zcash-swift-wallet-sdk at z-cale fork which adds getTreeState()
    for commitment tree witness generation. Revert this commit once the
    upstream PR (z-cale/zcash-swift-wallet-sdk#1) is merged.

--- modules/Package.swift
+++ modules/Package.swift
@@ -103,7 +103,7 @@ let package = Package(
         .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.7.2"),
         .package(url: "https://github.com/pointfreeco/swift-url-routing", from: "0.6.2"),
         .package(url: "https://github.com/zcash-hackworks/MnemonicSwift", from: "2.2.5"),
-        .package(url: "https://github.com/zcash/zcash-swift-wallet-sdk", from: "2.4.5"),
+        .package(url: "https://github.com/valargroup/zcash-swift-wallet-sdk.git", branch: "valargroup/governance-tree-state"),
         .package(url: "https://github.com/flexa/flexa-ios.git", exact: "1.1.4"),
         .package(url: "https://github.com/pacu/zcash-swift-payment-uri", from: "1.0.1"),
         .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.5.2"),
@@ -111,7 +111,7 @@ let package = Package(
         .package(url: "https://github.com/mgriebling/BigDecimal.git", from: Version(stringLiteral: "2.2.3")),
         .package(url: "https://github.com/siteline/swiftui-introspect", from: "26.0.0"),
         .package(url: "https://github.com/pointfreeco/swift-navigation", from: "2.5.1"),
-        .package(url: "https://github.com/z-cale/librustvoting.git", exact: "0.1.0")
+        .package(url: "https://github.com/valargroup/librustvoting.git", exact: "0.1.0")
     ],
     targets: [
         .target(
